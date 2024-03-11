5350bc4aa085ff9d58129d74d5da81c7dbd3a9ee
khumoyunhusanov <rashidovalutfiya@gmail.com>
2024-03-11 22:32:07 +0500

    console 10

---
+++ main.js
@@ -0,0 +1,18 @@
+// alert("salom")
+
+
+let a = 100;
+let user = 200;
+
+
+if (a < user) {
+  alert("good")
+} else {
+  alert("100 dan katta son yozing")
+  alert("100 dan katta son yozing")
+  alert("100 dan katta son yozing")
+  alert("100 dan katta son yozing")
+  alert("100 dan katta son yozing")
+  alert("100 dan katta son yozing")
+  
+}
